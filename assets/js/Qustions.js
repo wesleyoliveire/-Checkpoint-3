3e7df8c3474ce125e28cb04430be122de0727e3f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-17 22:02:57 -0300

    projeto

---
+++ assets/js/Qustions.js
@@ -0,0 +1,104 @@
+let questions = [
+    {
+        question: 'Em qual elemento HTML nós colocamos o javascript?',
+        options: [
+            'tag js',
+            'tag script',
+            'tag scripting',
+            'tag javascript'
+        ],
+        resposta: 1
+    },
+    {
+        question: 'Onde é o local certo de colocar o javascript?',
+        options: [
+            'Na tag body',
+            'Na tag head',
+            'Tanto na tag body quanto a head estão corretas'
+        ],
+        resposta: 2
+    },
+    {
+        question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
+        options: [
+            'name="xxx.js"',
+            'src="xxx.js"',
+            'href="xxx.js"'
+        ],
+        resposta: 1
+    },
+    {
+        question: 'Um arquivo javascript externo precisa ter a tag script',
+        options: [
+            'Verdade',
+            'Falso'
+        ],
+        resposta: 1
+    },
+    {
+        question: 'Como escrever um "Olá Mundo" em um alertbox?',
+        options: [
+            'alertBox("Olá Mundo")',
+            'msgBox("Olá Mundo")',
+            'msg("Olá Mundo")',
+            'alert("Olá Mundo")'
+        ],
+        resposta: 3
+    },
+    {
+        question: 'Como você cria uma função no Javascript?',
+        options: [
+            'function:minhaFuncao()',
+            'function = minhaFuncao()',
+            'function minhaFuncao()'
+        ],
+        resposta: 2
+    },
+    {
+        question: 'Como chamar uma função chamada "minhaFuncao"?',
+        options: [
+            'call minhaFuncao',
+            'call function minhaFuncao',
+            'minhaFuncao()'
+        ],
+        resposta: 2
+    },
+    {
+        question: 'Como escrever uma condicional IF no Javascript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5 then'
+        ],
+        resposta: 2
+    },
+    {
+        question: 'Como fazer um if que executa um código caso "i" for diferente de 5',
+        options: [
+            'if (i != 5)',
+            'if (i <> 5)',
+            'if i <> 5',
+            'if i =! 5 then'
+        ],
+        resposta: 0
+    },
+    {
+        question: 'Como o loop while começa?',
+        options: [
+            'while (i <= 10)',
+            'while (i <= 10; i++)',
+            'while i = 1 to 10'
+        ],
+        resposta: 0
+    },
+    {
+        question: 'Gostou do projeto?',
+        options: [
+            'Sim',
+            'Não',
+            'Talvez'
+        ],
+        resposta: 0
+    },
+];
